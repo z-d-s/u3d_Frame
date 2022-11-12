b1a7a9e887139437bb8b2612981d0cdf0843149a
zhangdiansong <lmtxzds@126.com>
2022-11-12 21:44:57 +0800

    同上

---
+++ Assets/Scripts/GameLaunch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameLaunch : UnitySingleton<GameLaunch>
+{
+    public override void Awake()
+    {
+        base.Awake();
+
+        #region 游戏框架的初始化
+        #endregion
+
+        #region 初始化游戏模块，然后进入游戏
+        this.gameObject.AddComponent<GameApp>();
+        #endregion
+    }
+
+    private void Start()
+    {
+        GameApp.Instance.EnterGame();
+    }
+}
